3093b3ecf81611f77f6451beafa92c4f1ca48fb0
BaridonFrancisco <baridonfrancisco@gmail.com>
2024-05-13 12:09:32 -0300

    inicializando proyecto

---
+++ src/main/java/com/EbookApi/apiEBook/ApiEBookApplication.java
@@ -0,0 +1,13 @@
+package com.EbookApi.apiEBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEBookApplication.class, args);
+	}
+
+}
